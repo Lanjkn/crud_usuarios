c0c69ee015b56b669b3be7c7c47cea2a2b59d4d1
beatriz <90482495+beatriztozzi@users.noreply.github.com>
2022-10-30 01:05:56 -0300

    criação do card controller
    criação do card controller

--- src/main/java/com/example/crud_usuarios/Cartao/controller/CardController.java
+++ src/main/java/com/example/crud_usuarios/Cartao/controller/CardController.java
@@ -1,5 +1,49 @@
 package com.example.crud_usuarios.Cartao.controller;
+
+import com.example.crud_usuarios.Cartao.model.CardModel;
+import com.example.crud_usuarios.Cartao.service.CardService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
 public class CardController {
 
+    private final CardService service;
+
+    @Autowired
+    public CardController(CardService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/cards")
+    public List<CardModel> getCards() {
+        return service.getCards();
+    }
+
+    @PostMapping("/cards")
+    public CardModel addCard(@RequestBody CardModel card) {
+        return service.addCard(card);
+    }
+
+    @DeleteMapping("/card_delete/{id}")
+    public void deleteCard(@PathVariable int id) {
+        service.deleteCard((long) id);
+    }
+
+    @PostMapping("/cards/{id}")
+    public CardModel updateCard(@RequestBody CardModel card, @PathVariable int id) {
+        return service.updateCard(card, (long) id);
+    }
+
+    @GetMapping("/cards/{id}")
+    public CardModel getCard(@PathVariable int id) {
+        return service.getCard((long) id);
+    }
+
+    public CardModel getCardByCardNumber(String cardNumber) {
+        return service.getCardByCardNumber(cardNumber);
+    }
 
 }
