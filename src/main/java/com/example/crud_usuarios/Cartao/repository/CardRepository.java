9a76f1afd07811154d19186f498de70f4196b5f1
beatriz <90482495+beatriztozzi@users.noreply.github.com>
2022-10-29 23:41:27 -0300

    criação card repositoty
    é isso

---
+++ src/main/java/com/example/crud_usuarios/Cartao/repository/CardRepository.java
@@ -0,0 +1,8 @@
+package com.example.crud_usuarios.Cartao.repository;
+
+import com.example.crud_usuarios.Cartao.model.CardModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CardRepository extends JpaRepository<CardModel, Long> {
+    CardModel findByEmail(String cardNumber);
+}
